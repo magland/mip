01569551deb8c9c7c711197380842bdb16fad8af
Jeremy Magland <jmagland@flatironinstitute.org>
2025-11-21 11:13:17 -0500

    disable kdtree mex

--- scripts/kdtree_compile_cpp14.m
+++ scripts/kdtree_compile_cpp14.m
@@ -1,68 +1,23 @@
 function kdtree_compile_cpp14()
-% kdtree_compile_cpp14
-% Build kdtree MEX files using MATLAB's own C++ compiler with -std=c++14
-%
-% This version avoids system GCC, avoids CXXFLAGS concatenation issues,
-% and ensures ABI compatibility with MATLAB’s libstdc++.
+% Custom compilation script for kdtree with C++14 standard
+localpath = fileparts(which('kdtree_compile'));
+fprintf(1,'Compiling kdtree library [%s] with C++14 standard...\n', localpath);
 
-    fprintf('--- kdtree MEX Build (C++14, MATLAB toolchain) ---\n');
+% Set C++14 standard flag
+cxxflags = 'CXXFLAGS="$CXXFLAGS -std=c++14"';
 
-    % Determine location of this script (kdtree/toolbox)
-    localpath = fileparts(which('kdtree_compile'));
-    fprintf('Building in: %s\n', localpath);
+err = 0;
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_build.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_delete.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_k_nearest_neighbors.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_ball_query.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_nearest_neighbor.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_range_query.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_io_from_mat.cpp']);
+err = err | mex(cxxflags, '-outdir', localpath, [localpath,'/kdtree_io_to_mat.cpp']);
 
-    %% Force MATLAB to use its own bundled GCC/G++
-    mex -setup C++;  % Load MATLAB's default toolchain
-
-    arch = computer('arch');
-    matlab_gxx = fullfile(matlabroot, 'bin', arch, 'g++');
-    matlab_gcc = fullfile(matlabroot, 'bin', arch, 'gcc');
-
-    fprintf('MATLAB C++ compiler: %s\n', matlab_gxx);
-
-    % Explicitly set CXX and CC inside MATLAB
-    setenv('CXX', matlab_gxx);
-    setenv('CC',  matlab_gcc);
-
-    %% List of source files
-    src_files = {
-        'kdtree_build.cpp'
-        'kdtree_delete.cpp'
-        'kdtree_k_nearest_neighbors.cpp'
-        'kdtree_ball_query.cpp'
-        'kdtree_nearest_neighbor.cpp'
-        'kdtree_range_query.cpp'
-        'kdtree_io_from_mat.cpp'
-        'kdtree_io_to_mat.cpp'
-    };
-
-    %% Build each MEX file
-    err = 0;
-
-    for k = 1:numel(src_files)
-        src = fullfile(localpath, src_files{k});
-        fprintf('\nCompiling %s...\n', src_files{k});
-
-        % Use ONLY MATLAB's compiler + C++14 flag
-        try
-            mex('-v', ...
-                ['CXX=' matlab_gxx], ...
-                ['CC='  matlab_gcc], ...
-                'CXXFLAGS=-std=c++14', ...
-                '-outdir', localpath, ...
-                src);
-        catch ME
-            warning("Error compiling %s:\n%s", src_files{k}, ME.message);
-            err = 1;
-        end
-    end
-
-    %% Check result
-    if err
-        error('One or more MEX files failed to compile.');
-    else
-        fprintf('\nAll kdtree MEX files compiled successfully.\n');
-    end
-
-    fprintf('--- Build complete ---\n');
+if err ~= 0
+   error('compile failed!');
+else
+   fprintf(1,'\bDone!\n');
 end
